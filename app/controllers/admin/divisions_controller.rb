8e739b786a8342c653199875514649e0535229ad
Darrin Holst <darrinholst@gmail.com>
2011-04-08 11:16:03 -0500

    use inherited resources for divisions

--- app/controllers/admin/divisions_controller.rb
+++ app/controllers/admin/divisions_controller.rb
@@ -1,37 +1,9 @@
 class Admin::DivisionsController < AdminController
-  def index
-    @divisions = Division.all
-  end
+  inherit_resources
 
-  def show
-    @division = Division.includes(:players).find(params[:id])
-  end
+  protected
 
-  def new
-    @division = Division.new
-  end
-
-  def edit
-    @division = Division.find(params[:id])
-  end
-
-  def create
-    @division = Division.new(params[:division])
-
-    if @division.save
-      redirect_to(admin_division_url(@division), :notice => 'Division was successfully created.')
-    else
-      render :action => "new"
-    end
-  end
-
-  def update
-    @division = Division.find(params[:id])
-
-    if @division.update_attributes(params[:division])
-      redirect_to(admin_division_url(@division), :notice => 'Division was successfully updated.')
-    else
-      render :action => "edit"
-    end
+  def resource
+    @division ||= end_of_association_chain.includes(:players).find(params[:id])
   end
 end
