8138eb0e7edeca43c58bcee90b563501075cfe89
Darrin Holst <darrinholst@gmail.com>
2011-04-08 11:19:52 -0500

    use inherited resources for games

--- app/controllers/admin/games_controller.rb
+++ app/controllers/admin/games_controller.rb
@@ -1,33 +1,10 @@
 class Admin::GamesController < AdminController
-  def index
-    @games = Game.order(:starts_at).all
-  end
+  inherit_resources
+  actions :all, :except => :show
 
-  def new
-    @game = Game.new
-  end
+  protected
 
-  def edit
-    @game = Game.find(params[:id])
-  end
-
-  def create
-    @game = Game.new(params[:game])
-
-    if @game.save
-      redirect_to(admin_games_url, :notice => 'Game was successfully created.')
-    else
-      render :action => "new"
-    end
-  end
-
-  def update
-    @game = Game.find(params[:id])
-
-    if @game.update_attributes(params[:game])
-      redirect_to(admin_games_url, :notice => 'Game was successfully updated.')
-    else
-      render :action => "edit"
-    end
+  def collection
+    @games ||= end_of_association_chain.order(:starts_at)
   end
 end
