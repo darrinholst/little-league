603bae0c4265c864c2d565d39a01bd447cc83cb2
Darrin Holst <darrinholst@gmail.com>
2011-04-08 11:18:11 -0500

    use inherited resources for fields

--- app/controllers/admin/fields_controller.rb
+++ app/controllers/admin/fields_controller.rb
@@ -1,33 +1,10 @@
 class Admin::FieldsController < AdminController
-  def index
-    @fields = Field.order(:name).all
-  end
+  inherit_resources
+  actions :all, :except => :show
 
-  def new
-    @field = Field.new
-  end
+  protected
 
-  def edit
-    @field = Field.find(params[:id])
-  end
-
-  def create
-    @field = Field.new(params[:field])
-
-    if @field.save
-      redirect_to(admin_fields_url, :notice => 'Field was successfully created.')
-    else
-      render :action => "new"
-    end
-  end
-
-  def update
-    @field = Field.find(params[:id])
-
-    if @field.update_attributes(params[:field])
-      redirect_to(admin_fields_url, :notice => 'Field was successfully updated.')
-    else
-      render :action => "edit"
-    end
+  def collection
+    @fields ||= end_of_association_chain.order(:name)
   end
 end
