69fa9e5d5293cd7cfbbfb22d683ae75abe44378a
Darrin Holst <darrinholst@gmail.com>
2011-04-16 21:09:26 -0500

    use start and end params from fullcalendar

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,6 +1,19 @@
+# [
+#   {
+#     "title": "Opening Ceremonies",
+#     "start": "<%= Timeliness.parse("04/30/2011 12:00:00", :zone => Time.zone).iso8601 %>",
+#     "allDay": false
+#   }
+# ]
 class CalendarController < ApplicationController
   def home
-    @games = Game.includes([{:home_team => :division}, :visiting_team]).order("divisions.name").all.map do |game|
+    @games = Game.includes([{:home_team => :division}, :visiting_team]).order("divisions.name")
+
+    if(params[:start] && params[:end])
+      @games = @games.where(['starts_at >= ? and starts_at <= ?', Time.at(params[:start].to_i), Time.at(params[:end].to_i)])
+    end
+
+    @games = @games.all.map do |game|
       {
         :title => "#{game.visiting_team.name} at #{game.home_team.name}",
         :start => game.starts_at.iso8601,
@@ -23,10 +36,3 @@ class CalendarController < ApplicationController
     end
   end
 end
-# [
-#   {
-#     "title": "Opening Ceremonies",
-#     "start": "<%= Timeliness.parse("04/30/2011 12:00:00", :zone => Time.zone).iso8601 %>",
-#     "allDay": false
-#   }
-# ]
