5b1f53aa49b39a651418ebd318e5eac1a4be00e4
Darrin Holst <darrinholst@gmail.com>
2012-04-10 10:10:17 -0500

    ga

--- config/application.rb
+++ config/application.rb
@@ -42,6 +42,8 @@ module Pll
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    config.assets.precompile += %w( ga.js )
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
