64a7808f43e905f3aef126aac53d715aeae82d4b
Darrin Holst <darrinholst@gmail.com>
2012-04-12 22:18:25 -0500

    fix spec

--- db/schema.rb
+++ db/schema.rb
@@ -14,63 +14,64 @@
 ActiveRecord::Schema.define(:version => 20120411003547) do
 
   create_table "coaches", :force => true do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "phone_number"
-    t.string   "email_address"
-    t.integer  "division_id"
-    t.integer  "team_id"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.string    "first_name"
+    t.string    "last_name"
+    t.string    "phone_number"
+    t.string    "email_address"
+    t.integer   "division_id"
+    t.integer   "team_id"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   create_table "divisions", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-    t.integer  "sort_order"
-    t.string   "color"
+    t.string    "name"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.integer   "sort_order"
+    t.string    "color"
   end
 
   create_table "fields", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string    "name"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   create_table "games", :force => true do |t|
-    t.datetime "starts_at"
-    t.integer  "visiting_team_id"
-    t.integer  "home_team_id"
-    t.integer  "field_id"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
+    t.timestamp "starts_at"
+    t.integer   "visiting_team_id"
+    t.integer   "home_team_id"
+    t.integer   "field_id"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   create_table "players", :force => true do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.integer  "division_id"
-    t.integer  "team_id"
-    t.datetime "created_at",             :null => false
-    t.datetime "updated_at",             :null => false
-    t.date     "birthdate"
-    t.string   "parents"
-    t.string   "phone_number"
-    t.string   "alternate_phone_number"
-    t.string   "shirt_size"
+    t.string    "first_name"
+    t.string    "last_name"
+    t.integer   "division_id"
+    t.integer   "team_id"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.date      "birthdate"
+    t.string    "parents"
+    t.string    "phone_number"
+    t.string    "alternate_phone_number"
+    t.string    "shirt_size"
   end
 
   create_table "teams", :force => true do |t|
-    t.string   "name"
-    t.integer  "division_id"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.string    "name"
+    t.integer   "division_id"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   create_table "users", :force => true do |t|
     t.string   "email",                                 :default => "",    :null => false
     t.string   "encrypted_password",     :limit => 128, :default => "",    :null => false
+    t.string   "password_salt",                         :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "remember_created_at"
     t.integer  "sign_in_count",                         :default => 0
@@ -78,8 +79,8 @@ ActiveRecord::Schema.define(:version => 20120411003547) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                               :null => false
-    t.datetime "updated_at",                                               :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.boolean  "admin",                                 :default => false
     t.datetime "reset_password_sent_at"
   end
