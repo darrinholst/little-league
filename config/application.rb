e7a690e07c0b002f44cbc2660281f502f92d4124
Darrin Holst <darrinholst@gmail.com>
2012-04-15 09:33:47 -0500

    add viewport settings

--- config/application.rb
+++ config/application.rb
@@ -42,7 +42,7 @@ module Pll
     # Enable the asset pipeline
     config.assets.enabled = true
 
-    config.assets.precompile += %w( ga.js )
+    config.assets.precompile += %w( modernizr.js ga.js )
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
