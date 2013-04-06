d215102547255d628b6979a7abd37bf6175cbddb
Darrin Holst <darrinholst@gmail.com>
2013-04-06 08:58:55 -0500

    don't show games yet

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,10 @@
 class HomeController < ApplicationController
   def index
-    @games = Game.current.by_date.all(:include => [
-      :field,
-      :visiting_team,
-      :home_team => [:division]
-    ])
+    #@games = Game.current.by_date.all(:include => [
+      #:field,
+      #:visiting_team,
+      #:home_team => [:division]
+    #])
+    @games = []
   end
 end
