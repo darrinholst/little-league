b8f33c80069254826bc27d63373bdcb87fb573b5
Darrin Holst <darrinholst@gmail.com>
2013-04-07 09:43:40 -0500

    show all games for admin

--- app/controllers/admin/games_controller.rb
+++ app/controllers/admin/games_controller.rb
@@ -5,8 +5,7 @@ class Admin::GamesController < AdminController
   protected
 
   def collection
-    @games ||= end_of_association_chain.order(:starts_at)
-                                       .includes([{:home_team => :division}, :visiting_team, :field])
-                                       .where("divisions.id" => params[:division])
+    @games ||= end_of_association_chain.order(:starts_at).includes([{:home_team => :division}, :visiting_team, :field])
+    @games = @games.where("divisions.id" => params[:division]) unless params[:division].blank?
   end
 end
