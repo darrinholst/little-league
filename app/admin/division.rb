05925522407ad95b8d6ef969593b7687c1aa4414
Darrin Holst <darrinholst@gmail.com>
2014-03-22 19:06:01 -0500

    clean up divisions

--- app/admin/division.rb
+++ app/admin/division.rb
@@ -1,4 +1,13 @@
 ActiveAdmin.register Division do
+  config.filters = false
+  config.sort_order = '!!!!GRRRR, I WILL JUST HANDLE IT MYSELF. GOOD FUCKING GOD!!!!'
+
+  controller do
+    def scoped_collection
+      Division.order('sort_order desc')
+    end
+  end
+
   index do
     column :name
     default_actions
