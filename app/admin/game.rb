b1a84de9731b18834b95fab5cfc2a035f2f4861c
Darrin Holst <darrinholst@gmail.com>
2014-04-15 21:32:25 -0500

    activeadmin game filtering and cusomized csv export

--- app/admin/game.rb
+++ app/admin/game.rb
@@ -2,9 +2,13 @@ ActiveAdmin.register Game do
   permit_params :visiting_team_id, :home_team_id, :field_id, :starts_at
   actions :index, :new, :create, :update, :edit, :destroy
 
+  # allows us to initially use the multiple column sort order below
+  # https://github.com/gregbell/active_admin/blob/bc791b3e3cf8084e4d9644650cbc7570b97a801a/lib/active_admin/resource_controller/data_access.rb#L220
+  config.sort_order = '!'
+
   controller do
     def scoped_collection
-      Game.includes(:field, :home_team, :visiting_team)
+      Game.includes(:field, {:home_team => [:division]}, :visiting_team).order('starts_at, home_team.division.sort_order')
     end
   end
 
@@ -19,6 +23,7 @@ ActiveAdmin.register Game do
   filter :visiting_team
   filter :home_team
   filter :field
+  filter :home_team_local, as: :boolean, label: 'In Perry?'
 
   form do |f|
     f.semantic_errors
@@ -31,5 +36,13 @@ ActiveAdmin.register Game do
     end
 
     f.actions
+  end
+
+  csv do
+    column('Time') {|g| g.starts_at_display}
+    column('Division') {|g| g.home_team.division.name}
+    column('Field') {|g| g.field.name}
+    column('Visiting Team') {|g| g.visiting_team.name}
+    column('Home Team') {|g| g.home_team.name}
   end
 end
