0e4c453cf3ec88c32b7367ed8b8f2e85c84230b3
Darrin Holst <darrinholst@gmail.com>
2017-02-12 10:09:22 -0600

    environment variable for motd

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def motd
-    'Closing Ceremonies will be held at 2pm on Sunday, June 12th at the Majors Field' if Time.now.utc < Chronic.parse('6/12/2016 4pm').utc
+    ENV['MOTD'] || ('Closing Ceremonies will be held at 2pm on Sunday, June 12th at the Majors Field' if Time.now.utc < Chronic.parse('6/12/2016 4pm').utc)
   end
 end
