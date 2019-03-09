3c64c21f7839d581e110155a2a38b6fa542c8210
Darrin Holst <darrinholst@gmail.com>
2019-03-09 09:41:44 -0600

    disable dynamic assets compiling

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Pll::Application.configure do
   config.action_controller.perform_caching = true
   config.serve_static_assets = false
   config.assets.compress = true
-  config.assets.compile = true
+  config.assets.compile = false
   config.assets.digest = true
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
